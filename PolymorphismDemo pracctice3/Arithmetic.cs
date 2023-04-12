c2c8bba0d762e86899d2ea599024e4ef4f513dee
HP <HP@DESKTOP-4Q8V1JF>
2023-04-12 18:45:20 +0530

    Add project files.

---
+++ PolymorphismDemo pracctice3/Arithmetic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismDemo_pracctice3
+{
+    internal class Arithmetic
+    {
+        public void Add()
+        {
+            Console.WriteLine("method with 0 argrument");
+        }
+        public void Add(int num1)
+        {
+            Console.WriteLine("Methods with 1 parameter");
+        }
+        public void Add(int num1,int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("Addition of two integers types value= " + result);
+        }
+        public void Add(float num1,float num2) 
+        { 
+           float result = num1 + num2;
+            Console.WriteLine("Addition of two float types value=" + result);
+        }
+        public void Add(float num1,int num2)
+        {
+            float result = num1 + num2;
+            Console.WriteLine("Addition of one float value and one integers value=" + result);
+        }
+        public void Add(int num1 , float num2)
+        {
+           float result = num1 + num2;
+            Console.WriteLine("Additio of one integer value and one float value=" + result);
+
+        }
+    }
+}
---
+++ PolymorphismDemo pracctice3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PolymorphismDemo_pracctice3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Arithmetic arithmetic = new Arithmetic();
+            arithmetic.Add();
+            arithmetic.Add(10);
+            arithmetic.Add(10,20);
+            arithmetic.Add(20.5f,30.5f);
+            arithmetic.Add(30.5f, 25);
+            arithmetic.Add(40, 50.5f);
+        }
+    }
+}
